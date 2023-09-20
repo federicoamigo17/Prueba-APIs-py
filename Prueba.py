2fab668bae7456de7c64db2a20c4407dcbe76980
federicoamigo17 <federicoamigo17@gmail.com>
2023-09-20 16:51:02 -0300

    primer prueba fallida error en la api

--- Prueba.py
+++ Prueba.py
@@ -1,2 +1,20 @@
-print("hola mundo")
+import requests
+import json
+import pandas as pd
 
+url = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all"
+
+respuesta = requests.get(url)
+
+# estoy teniendo problemas para levantar la api por lo tanto agrego una verificacion para ver que pasa
+
+if respuesta.status_code == 200:
+    try:
+        datosperros= respuesta.json()
+        facto_perro= data[0]["fact"]
+        dfperros = pd.DataFrame({"facto": [facto_perro]})
+        print(dfperros.head())
+    except json.JSONDecodeError as e:
+        print("Error en el JSON", str(e))
+else:
+    print("El error esta en la API .Codigo:", respuesta.status_code)
